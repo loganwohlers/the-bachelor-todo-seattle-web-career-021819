8aca322f7a2ec3db51dbd89f00422907a9bae606
Logan Wohlers <lawohlers@gmail.com>
2019-02-08 22:41:57 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -5,7 +5,9 @@ def get_first_name_of_season_winner(data, season)
         h.each do |k2, v2|
           puts k2
           puts v2
-          
+          if v2=="Winner"
+            return data[k][v][h]  
+          end
         end
       end
     end
